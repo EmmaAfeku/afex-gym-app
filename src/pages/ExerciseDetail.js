1ed60902cd1117ba9465bab39e3325f36197a815
Emmanuel Afeku <emmanuelafeku@gmail.com>
2023-11-18 22:50:03 +0000

    initial Commit for Exercise details page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import fetchData, { exerciseOptions } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+   return (
+      <Box>
+         <Detail />
+         <ExerciseVideos />
+         <SimilarExercises />
+      </Box>
+   );
+};
 
-export default ExerciseDetail
+export default ExerciseDetail;
