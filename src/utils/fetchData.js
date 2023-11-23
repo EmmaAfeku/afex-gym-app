a5b8dca038c1db743958e4342bb5d713899bd678
Emmanuel Afeku <emmanuelafeku@gmail.com>
2023-11-23 01:56:59 +0000

    Hiding APi key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
 	method: 'GET',
 	url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
 	headers: {
-		'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
 		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
 	},
 };
